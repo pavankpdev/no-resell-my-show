28a3931d5c74f933b411bb0010392ef93884eae5
pavanleton <pavankumarkpkp@gmail.com>
2024-10-04 22:56:41 +0530

    setup gas reporter

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,8 +1,16 @@
 import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
+import "hardhat-gas-reporter"
 
 const config: HardhatUserConfig = {
   solidity: "0.8.27",
+  gasReporter: {
+    enabled: true,
+    outputFile: "gas-report.txt",
+    noColors: true,
+    // currency: "USD",
+    // coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+  }
 };
 
 export default config;

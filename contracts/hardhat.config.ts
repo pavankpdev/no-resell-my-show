1f94b179f7bfabb35d15e428e3049f71ea871bd5
pavanleton <pavankumarkpkp@gmail.com>
2024-10-04 22:57:42 +0530

    setup solidity-coverage

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,6 +1,7 @@
 import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
 import "hardhat-gas-reporter"
+import 'solidity-coverage'
 
 const config: HardhatUserConfig = {
   solidity: "0.8.27",
